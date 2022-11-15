4358d6e3d456743a64213650adbe05ec3f41bbee
jamiltarek55 <77003619+jamiltarek55@users.noreply.github.com>
2022-11-15 23:32:01 +0600

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,7 +2,6 @@ import Link from 'next/link';
 import React from 'react';
 import { AiFillGithub, AiFillMail, AiFillLinkedin } from 'react-icons/ai';
 import { DiCssdeck } from 'react-icons/di';
-
 import { Container, Div1, Div2, Div3, NavLink, SocialIcons } from './HeaderStyles';
 
 const Header = () =>  (
@@ -44,5 +43,4 @@ const Header = () =>  (
       </Div3>
     </Container>
 );
-
 export default Header;
