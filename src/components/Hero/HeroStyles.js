fc5767380d1a99399312eb6e32b2df021c4afe5d
jamiltarek55 <77003619+jamiltarek55@users.noreply.github.com>
2022-11-15 23:36:41 +0600

    Update HeroStyles.js

--- src/components/Hero/HeroStyles.js
+++ src/components/Hero/HeroStyles.js
@@ -1,17 +1,17 @@
 import styled from 'styled-components';
 
 export const GridContainer = styled.section`
-display: grid;
-grid-template-columns: repeat(auto-fill, minmax(500px, 1fr));
-padding: -rem;
-place-items: left;
-column-gap: 2rem;
-row-gap: -1rem;
-@media ${(props) => props.theme.breakpoints.sm} {
-  display: flex;
-  flex-direction: column;
-  padding: 2rem;
-  padding-bottom: -1;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(500px, 1fr));
+  padding: -rem;
+  place-items: left;
+  column-gap: 2rem;
+  row-gap: -1rem;
+  @media ${(props) => props.theme.breakpoints.sm} {
+    display: flex;
+    flex-direction: column;
+    padding: 2rem;
+    padding-bottom: -1;
 }
 `;
 
@@ -33,19 +33,17 @@ export const Img = styled.img`
 `;
 
 export const LeftSection = styled.div `
-width: 100%;
-  @media ${(props) => props.theme.breakpoints.sm} {
-    width: 80%;
-    display: flex;
-    flex-direction: column;
-
-    margin: 3 auto;
-  }
-  @media ${(props) => props.theme.breakpoints.md} {
     width: 100%;
-    display: flex;
-    flex-direction: column;
-
-    margin: 0 auto;
+    @media ${(props) => props.theme.breakpoints.sm} {
+      width: 80%;
+      display: flex;
+      flex-direction: column;
+      margin: 3 auto;
+    }
+    @media ${(props) => props.theme.breakpoints.md} {
+      width: 100%;
+      display: flex;
+      flex-direction: column;
+      margin: 0 auto;
   }
 `;
