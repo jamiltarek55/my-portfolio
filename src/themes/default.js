a96852fa4f9f61a865bc8d46c6470e96af51df6f
jamiltarek55 <77003619+jamiltarek55@users.noreply.github.com>
2022-11-15 23:23:59 +0600

    Update default.js

--- src/themes/default.js
+++ src/themes/default.js
@@ -4,6 +4,7 @@ export default {
     title: "Space Grotesk, sans-serif",
     main: "Space Grotesk, sans-serif"
   },
+  
   // Colors for layout
   colors: {
     primary1: "hsl(204,23.8%,95.9%)",
@@ -12,6 +13,7 @@ export default {
     button: "hsl(205.1,100%,36.1%)",
     background2: "hsl(232.7,27.3%,23.7%)",
   },
+  
   // Breakpoints for responsive design
   breakpoints: {
     sm: 'screen and (max-width: 380px)',
